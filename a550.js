4e5b2af0fe75baa4b5a6622de907bdeaf628f4b4
mohrly123 <168989632+mohrly123@users.noreply.github.com>
2024-10-11 23:07:23 +0200

    Update a550.js

--- a550.js
+++ a550.js
@@ -2,34 +2,36 @@
 const btnBerechnenAbstand = document.getElementById("btnBerechnenAbstand");
 
 
-// Funktion zum berechnen des Abstandes
+
+//------------------------- Abstandsbereich ------------------------------
+
 function abstand550(){
-    let eingabeA = document.getElementById("inputIstAbstand").value
-    eingabeA = Number(eingabeA)
-    const sollabstand = 1665
-    const minabstand = 1600
-    const maxabstand = 1700
+    let eingabeA = document.getElementById("inputIstAbstand").value;
+    eingabeA = Number(eingabeA);
+    const sollabstand = 1665;
+    const minabstand = 1600;
+    const maxabstand = 1700;
     // Wenn die Eingabe größer als der Mindestabstand und kleiner als der Max Abstand ist dann geht es weiter
     if(eingabeA >= minabstand && eingabeA <= maxabstand){
         // Wenn der IST Abstand kleiner als der Sollabstand ist dann gehört das Gleis weg vom Bahnsteig
         if(eingabeA < sollabstand){
-            let ergebnis = sollabstand - eingabeA
-            document.getElementById("resultAbstand550").textContent = `Das Gleis muss um  ${ergebnis} mm weg vom Bahnsteig!`
-            document.getElementById("inputIstAbstand").value = ""
-            setTimeout(textVerschwinden, 6000)
+            let ergebnis = sollabstand - eingabeA;
+            document.getElementById("resultAbstand550").textContent = `Das Gleis muss um  ${ergebnis} mm weg vom Bahnsteig!`;
+            document.getElementById("inputIstAbstand").value = "";
+            setTimeout(textVerschwinden, 6000);
         }
         // Wenn der IST Abstand größer als der Sollabstand ist dann gehört das Gleis zum vom Bahnsteig
         else if(eingabeA > sollabstand){
-            document.getElementById("resultAbstand550").textContent = `Das Gleis muss um ${ergebnis} mm zum Bahnsteig!`
-            document.getElementById("inputIstAbstand").value = ""
-            setTimeout(textVerschwinden, 6000)
+            document.getElementById("resultAbstand550").textContent = `Das Gleis muss um ${ergebnis} mm zum Bahnsteig!`;
+            document.getElementById("inputIstAbstand").value = "";
+            setTimeout(textVerschwinden, 6000);
         }else{
-            document.getElementById("resultAbstand550").textContent = `Das Gleis hat 0 mm Verschiebung`
-            document.getElementById("inputIstAbstand").value = ""
-            setTimeout(textVerschwinden, 6000)
+            document.getElementById("resultAbstand550").textContent = `Das Gleis hat 0 mm Verschiebung`;
+            document.getElementById("inputIstAbstand").value = "";
+            setTimeout(textVerschwinden, 6000);
         }
     } else{
-        alert("Achtung IHP RW 06.01.01 Verletzung!! Techniker verständigen!")
+        alert("Achtung IHP RW 06.01.01 Verletzung!! Techniker verständigen!");
     }
 
 }
@@ -38,13 +40,26 @@ function abstand550(){
 // ------------------- Höhenbereich -----------------------
 
 function höhe550(){
-    
+    let eingabeH = document.getElementById("inputIstHöhe").value;
+    const sollhöhe = 550;
+    const minhöhe = 500;
+    const maxhöhe = 600;
+    if (eingabeH > minhöhe && eingabeH < maxhöhe){
+        if (eingabeH < sollhöhe){
+            let ergebnis = sollhöhe - eingabeH;
+            ergebnis = Number(ergebnis);
+            document.getElementById("resultHöhe550").textContent = `Das Gleis ist um ${ergebnis} mm zu hoch!`;
+            document.getElementById("inputIstHöhe").value = "";
+            setTimeout(textVerschwinden,6000)
+        }
+    }
 }
 
 
 
 // Funktion um den Text verschwinden zu lassen
 function textVerschwinden(){
-    document.getElementById("resultAbstand550").textContent = ""
+    document.getElementById("resultAbstand550").textContent = "";
+    document.getElementById("resultHöhe550").textContent = ";"
 }
 
